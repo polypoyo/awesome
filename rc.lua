6912c20df53c98d77e6bb19f09963d15e36d6917
polypoyo <evanpaulbishop@gmail.com>
2022-06-14 20:28:45 -0400

    Moved theme to it's own folder

--- rc.lua
+++ rc.lua
@@ -6,7 +6,7 @@ pcall(require, 'luarocks.loader')
 -- load theme
 local beautiful = require'beautiful'
 local gears = require'gears'
-beautiful.init(gears.filesystem.get_configuration_dir() .. 'theme.lua')
+beautiful.init(gears.filesystem.get_configuration_dir() .. 'theme/init.lua')
 
 -- load key and mouse bindings
 require'bindings'
